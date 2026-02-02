bc89655499b4010d9eacd4ce58154922cd729f8a
kosuke sugiura <kosuke@kosukenoMacBook-Pro.local>
2026-02-02 12:12:42 +0900

    Use local mountain assets

---
+++ CREDITS.md
@@ -0,0 +1,94 @@
+# Image Credits
+
+Images used in this app are from Wikimedia Commons.
+
+- 槍ヶ岳 — Mount_Yari_Abendrot_from_Mount_Otensho.jpg
+  - Author: Alpsdake
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Yari_Abendrot_from_Mount_Otensho.jpg
+
+- 奥穂高岳 — Mountain_trail_to_Mt._Okuho.jpg
+  - Author: Draupnir3
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AMountain_trail_to_Mt._Okuho.jpg
+
+- 剱岳 — Mount_Tsurugi_Glacier.jpg
+  - Author: Alpsdake
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Tsurugi_Glacier.jpg
+
+- 立山（雄山） — Oyama,_Tateyama.jpg
+  - Author: (unknown)
+  - License: CC BY-SA 3.0
+  - License URL: http://creativecommons.org/licenses/by-sa/3.0/
+  - Source: https://commons.wikimedia.org/wiki/File%3AOyama%2C_Tateyama.jpg
+
+- 白馬岳 — Sirouma.JPG
+  - Author: Σ64
+  - License: CC BY 3.0
+  - License URL: https://creativecommons.org/licenses/by/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3ASirouma.JPG
+
+- 唐松岳 — Karamatsudake_chojosanso.jpg
+  - Author: Alpsdake
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AKaramatsudake_chojosanso.jpg
+
+- 常念岳 — Mount_Jyonen_from_Azusa_River_1996-04-29.jpg
+  - Author: alpsdake
+  - License: CC0
+  - License URL: http://creativecommons.org/publicdomain/zero/1.0/deed.en
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Jyonen_from_Azusa_River_1996-04-29.jpg
+
+- 燕岳 — 燕岳_Mount_Tsubakuro_(139441879).jpeg
+  - Author: Ganden
+  - License: CC BY 3.0
+  - License URL: https://creativecommons.org/licenses/by/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3A%E7%87%95%E5%B2%B3_Mount_Tsubakuro_%28139441879%29.jpeg
+
+- 乗鞍岳 — Mount_Norikura_Kengamine.jpg
+  - Author: Alpsdake
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Norikura_Kengamine.jpg
+
+- 笠ヶ岳 — Hida_Mountains_from_Mount_Kasa_2002-8-29.jpg
+  - Author: Alpsdake
+  - License: CC0
+  - License URL: http://creativecommons.org/publicdomain/zero/1.0/deed.en
+  - Source: https://commons.wikimedia.org/wiki/File%3AHida_Mountains_from_Mount_Kasa_2002-8-29.jpg
+
+- 薬師岳 — Mount_Yakushi_from_Mount_Mitsumata_renge_2000-08-17.jpg
+  - Author: Alpsdake
+  - License: CC0
+  - License URL: http://creativecommons.org/publicdomain/zero/1.0/deed.en
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Yakushi_from_Mount_Mitsumata_renge_2000-08-17.jpg
+
+- 黒部五郎岳 — Mount_Kurobegoro.jpg
+  - Author: Inti-sol
+  - License: CC BY-SA 3.0
+  - License URL: http://creativecommons.org/licenses/by-sa/3.0/
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Kurobegoro.jpg
+
+- 双六岳 — Mount_Sugoroku.JPG
+  - Author: Alpsdake
+  - License: CC BY-SA 3.0
+  - License URL: https://creativecommons.org/licenses/by-sa/3.0
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Sugoroku.JPG
+
+- 大天井岳 — View_from_Mt.Otenshodake_04.jpg
+  - Author: Σ64
+  - License: CC BY-SA 3.0
+  - License URL: http://creativecommons.org/licenses/by-sa/3.0/
+  - Source: https://commons.wikimedia.org/wiki/File%3AView_from_Mt.Otenshodake_04.jpg
+
+- 五竜岳 — Mount_Goryu_1996-01-02.jpg
+  - Author: Alpsdake
+  - License: CC0
+  - License URL: http://creativecommons.org/publicdomain/zero/1.0/deed.en
+  - Source: https://commons.wikimedia.org/wiki/File%3AMount_Goryu_1996-01-02.jpg
+
--- lib/main.dart
+++ lib/main.dart
@@ -21,282 +21,25 @@ class SimpleTrainingApp extends StatefulWidget {
   State<SimpleTrainingApp> createState() => _SimpleTrainingAppState();
 }
 
-const String appTitle = '鳥獣判別トレーニング';
+const String appTitle = '北アルプス山名トレーニング';
 
 class _SimpleTrainingAppState extends State<SimpleTrainingApp> {
   final List<List<String>> data = [
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/YBIADAcir8.jpg",
-      "〇 マガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/GjoZKbqOPV.jpg",
-      "〇 カルガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/spl8RSSXap.jpg",
-      "〇 オナガガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/8pVyHmOxRg.jpg",
-      "〇 ヒドリガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/50ohNhK8eO.jpg",
-      "〇 ハシビロガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/NzrwKUFp3M.jpg",
-      "〇 ヨシガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/xiv2bTYyoh.jpg",
-      "〇 コガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/Ti8ry0sgsU.jpg",
-      "〇 ホシハジロ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/8Rqm61mpEw.jpg",
-      "〇 スズガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/l986c7jmS7.jpg",
-      "〇 キンクロハジロ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/tHlnOyXiOJ.jpg",
-      "〇 クロガモ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/mvn01q8fW3.jpg",
-      "〇 ヤマシギ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/7NeHAWkedI.jpg",
-      "〇 タシギ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/ndZxqAc3qj.jpg",
-      "〇 カワウ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/jqMvcBuj04.jpg",
-      "〇 エゾライチョウ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/2IUXXC5ggm.jpg",
-      "〇 コジュケイ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/hrpFLa73Rl.jpg",
-      "〇 ヤマドリ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/iYOgAGWiDn.jpg",
-      "〇 キジバト",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/PAuTzDU4Rz.jpg",
-      "〇 キジ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/PgU3iMzf9K.jpg",
-      "〇 ニュウナイスズメ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/W3gWJzzPz3.jpg",
-      "〇 ヒヨドリ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/1MqYEmAa4f.jpg",
-      "〇 スズメ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/2oBsMAdFrh.jpg",
-      "〇 ムクドリ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/vfEy30rMv5.jpg",
-      "〇 ミヤマガラス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/plM2SfDsHL.jpg",
-      "〇 ハシブトガラス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/p708NUH8Nb.jpg",
-      "〇 ハシボソガラス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/wU1zzBLtOs.jpg",
-      "〇 ノウサギ・ユキウサギ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/6btvfSijiv.jpg",
-      "〇 タイワンリス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/P0DGzAMDHI.jpg",
-      "〇 シマリス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/w3fYetFJU2.jpg",
-      "〇 ヌートリア",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/NbGv7kf793.jpg",
-      "〇 ヒグマ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/GW2oUdfXdM.jpg",
-      "〇 ツキノワグマ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/xx1pn7zGiD.jpg",
-      "〇 イノシシ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/UKh4X3u152.jpg",
-      "〇 ニホンジカ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/3VBfHVYNUt.jpg",
-      "〇 アライグマ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/4plsMuBMH4.jpg",
-      "〇 キツネ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/HbdEpwpX2R.jpg",
-      "〇 テン",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/ebW8wf6RMT.jpg",
-      "〇 イタチのオス",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/kkbkz1xQdC.jpg",
-      "〇 ミンク",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/snPUBKCRGE.jpg",
-      "〇 アナグマ",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/rBGmVhd7tT.jpg",
-      "〇 ハクビシン",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/fbKPDx7CLS.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/KyUZPXwZ1z.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/dVkVwENDgK.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/ahc6pEfrth.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/0ZIOmcvaUr.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/4RNmf5WOGE.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/SaQQCepUJe.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/w292eD8JyA.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/e0q1fLIfu1.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/wWbPhwtbe0.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/DyUM0RWtps.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/tEkg4sxuEh.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/wpgaBvscoF.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/T13PZFhATj.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/3LfrCnyFur.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/R86cmbktXP.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/oQOHigAjrJ.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/VHqzVtSOU4.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/P80xwK0oBB.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/1cSRPhbCVl.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/ytfRDIL7n8.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/jgn3WsN8ok.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/gH4jOmVbXq.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/UybpiHbuUm.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/hZjF0h9zJe.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/iFDkBQDi6t.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
-    [
-      "https://raw.githubusercontent.com/kmc2400/hunting_exam/refs/heads/main/cfCulOmnOT.jpg",
-      "✕ 非狩猟鳥獣",
-    ],
+    ["assets/mountains/yari.jpg", "槍ヶ岳"],
+    ["assets/mountains/okuho.jpg", "奥穂高岳"],
+    ["assets/mountains/tsurugi.jpg", "剱岳"],
+    ["assets/mountains/oyama_tateyama.jpg", "立山（雄山）"],
+    ["assets/mountains/shirouma.jpg", "白馬岳"],
+    ["assets/mountains/karamatsu.jpg", "唐松岳"],
+    ["assets/mountains/jonen.jpg", "常念岳"],
+    ["assets/mountains/tsubakuro.jpg", "燕岳"],
+    ["assets/mountains/norikura.jpg", "乗鞍岳"],
+    ["assets/mountains/kasa.jpg", "笠ヶ岳"],
+    ["assets/mountains/yakushi.jpg", "薬師岳"],
+    ["assets/mountains/kurobegoro.jpg", "黒部五郎岳"],
+    ["assets/mountains/sugoroku.jpg", "双六岳"],
+    ["assets/mountains/otensho.jpg", "大天井岳"],
+    ["assets/mountains/goryu.jpg", "五竜岳"],
   ];
 
   late final List<Sample> _samples;
@@ -344,13 +87,10 @@ class _SimpleTrainingAppState extends State<SimpleTrainingApp> {
         child: Stack(
           children: [
             Positioned.fill(
-              child: Image.network(
+              child: Image.asset(
                 sample.imageUrl,
                 // fit: BoxFit.cover,// 画面いっぱいに広げる代わりに上下左右が切れる
                 fit: BoxFit.contain, // 画像全体が必ず画面内に収まる（余白が出ることはあるが切れない）
-                loadingBuilder: (c, w, p) => p == null
-                    ? w
-                    : const Center(child: CircularProgressIndicator()),
                 errorBuilder: (e1, e2, e3) =>
                     const Center(child: Text('画像を読み込めません')),
               ),
