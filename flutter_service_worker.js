5bf5710e036685e0921f90d950bff264a629640d
kosuke sugiura <kosuke@kosukenoMacBook-Pro.local>
2026-02-02 10:27:29 +0900

    Deploy web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c899cf72cde29cf49185e46b95b98181",
+const RESOURCES = {"flutter_bootstrap.js": "317056f67afaf111684ae416e781fbad",
 "version.json": "51515c9ed4d94f6ddb10608d83ee52c9",
 "index.html": "ce4f7c3abb9c62236f8582cca7f3d248",
 "/": "ce4f7c3abb9c62236f8582cca7f3d248",
